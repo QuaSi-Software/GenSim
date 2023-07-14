b432d498a9fa92e2c2906700b3a3b06515ffcae0
Etienne Ott <etienne.ott@siz-energieplus.de>
2023-07-14 14:21:36 +0200

    Fix several issues of measure CustomExportParams preventing successful run.

--- Measures/CustomExportParams/measure.rb
+++ Measures/CustomExportParams/measure.rb
@@ -24,81 +24,81 @@ class CustomExportParams < OpenStudio::Measure::ModelMeasure
     def arguments(model)
         args = OpenStudio::Measure::OSArgumentVector.new
 
-        has_zone_details = OpenStudio::Ruleset::OSArgument::makeIntegerArgument("zoneDetails", true)
-        has_zone_details.setDisplayName("Has zone details?")
+        has_zone_details = OpenStudio::Measure::OSArgument::makeIntegerArgument("zoneDetails", false)
+        has_zone_details.setDisplayName("Has zone details")
         args << has_zone_details
 
-        do_detailed_hvac = OpenStudio::Ruleset::OSArgument::makeIntegerArgument("detailedHVAC", true)
-        do_detailed_hvac.setDisplayName("With detailed HVAC?")
+        do_detailed_hvac = OpenStudio::Measure::OSArgument::makeIntegerArgument("detailedHVAC", false)
+        do_detailed_hvac.setDisplayName("With detailed HVAC")
         args << do_detailed_hvac
 
-        ventilation_type = OpenStudio::Ruleset::OSArgument::makeIntegerArgument("ventilation_type", true)
+        ventilation_type = OpenStudio::Measure::OSArgument::makeIntegerArgument("ventilation_type", false)
         ventilation_type.setDisplayName("Ventilation type")
         args << ventilation_type
 
-        floor_length = OpenStudio::Ruleset::OSArgument::makeDoubleArgument("floor_length", true)
+        floor_length = OpenStudio::Measure::OSArgument::makeDoubleArgument("floor_length", false)
         floor_length.setDisplayName("Floor length")
         floor_length.setUnits("m")
         args << floor_length
 
-        floor_width = OpenStudio::Ruleset::OSArgument::makeDoubleArgument("floor_width", true)
+        floor_width = OpenStudio::Measure::OSArgument::makeDoubleArgument("floor_width", false)
         floor_width.setDisplayName("Floor width")
         floor_width.setUnits("m")
         args << floor_width
 
-        ratio_NRF_BGF_selection = OpenStudio::Ruleset::OSArgument::makeStringArgument("ratio_NRF_BGF", true)
-        ratio_NRF_BGF_selection.setDisplayName("Ratio of NRF over BGF (selection)")
+        ratio_NRF_BGF_selection = OpenStudio::Measure::OSArgument::makeStringArgument("ratio_NRF_BGF", false)
+        ratio_NRF_BGF_selection.setDisplayName("Ratio of NRF over BGF selection")
         args << ratio_NRF_BGF_selection
 
-        ratio_NRF_BGF = OpenStudio::Ruleset::OSArgument::makeDoubleArgument("NRF_BGF", true)
+        ratio_NRF_BGF = OpenStudio::Measure::OSArgument::makeDoubleArgument("NRF_BGF", false)
         ratio_NRF_BGF.setDisplayName("Ratio of NRF over BGF")
         args << ratio_NRF_BGF
 
-        holidays = OpenStudio::Ruleset::OSArgument::makeStringArgument("Holidays", true)
+        holidays = OpenStudio::Measure::OSArgument::makeStringArgument("Holidays", false)
         holidays.setDisplayName("Definition of holidays")
         args << holidays
 
-        calc_feed_air = OpenStudio::Ruleset::OSArgument::makeIntegerArgument("vorlauf_Luft", true)
-        calc_feed_air.setDisplayName("Vorlauf Luft berechnen?")
+        calc_feed_air = OpenStudio::Measure::OSArgument::makeIntegerArgument("vorlauf_Luft", false)
+        calc_feed_air.setDisplayName("Vorlauf Luft berechnen")
         args << calc_feed_air
 
-        calc_return_air = OpenStudio::Ruleset::OSArgument::makeIntegerArgument("nachlauf_Luft", true)
-        calc_return_air.setDisplayName("Rücklauf Luft berechnen?")
+        calc_return_air = OpenStudio::Measure::OSArgument::makeIntegerArgument("nachlauf_Luft", false)
+        calc_return_air.setDisplayName("Rücklauf Luft berechnen")
         args << calc_return_air
 
-        calc_shading_control = OpenStudio::Ruleset::OSArgument::makeIntegerArgument("Shadingcontrol", true)
-        calc_shading_control.setDisplayName("Sonnenschutz berechnen?")
+        calc_shading_control = OpenStudio::Measure::OSArgument::makeIntegerArgument("Shadingcontrol", false)
+        calc_shading_control.setDisplayName("Sonnenschutz berechnen")
         args << calc_shading_control
 
-        calc_infiltration = OpenStudio::Ruleset::OSArgument::makeIntegerArgument("Infiltration", true)
-        calc_infiltration.setDisplayName("Infiltration berechnen?")
+        calc_infiltration = OpenStudio::Measure::OSArgument::makeIntegerArgument("Infiltration", false)
+        calc_infiltration.setDisplayName("Infiltration berechnen")
         args << calc_infiltration
 
-        calc_lighting_control = OpenStudio::Ruleset::OSArgument::makeIntegerArgument("Lightingcontrol", true)
-        calc_lighting_control.setDisplayName("Lichtsteuerung berechnen?")
+        calc_lighting_control = OpenStudio::Measure::OSArgument::makeIntegerArgument("Lightingcontrol", false)
+        calc_lighting_control.setDisplayName("Lichtsteuerung berechnen")
         args << calc_lighting_control
 
-        calc_window_vent = OpenStudio::Ruleset::OSArgument::makeIntegerArgument("Windowvent", true)
-        calc_window_vent.setDisplayName("Fensterlüftung berechnen?")
+        calc_window_vent = OpenStudio::Measure::OSArgument::makeIntegerArgument("Windowvent", false)
+        calc_window_vent.setDisplayName("Fensterlüftung berechnen")
         args << calc_window_vent
 
-        is_generic_geom = OpenStudio::Ruleset::OSArgument::makeIntegerArgument("genGeom", true)
-        is_generic_geom.setDisplayName("Generische Geometrie?")
+        is_generic_geom = OpenStudio::Measure::OSArgument::makeIntegerArgument("genGeom", false)
+        is_generic_geom.setDisplayName("Generische Geometrie")
         args << is_generic_geom
 
-        cond_floor_height = OpenStudio::Ruleset::OSArgument::makeDoubleArgument("FloorHeight_kond", true)
+        cond_floor_height = OpenStudio::Measure::OSArgument::makeDoubleArgument("FloorHeight_kond", false)
         cond_floor_height.setDisplayName("Konditionierte Raumhöhe")
         cond_floor_height.setUnits("m")
         args << cond_floor_height
 
-        imported_floor_height = OpenStudio::Ruleset::OSArgument::makeDoubleArgument("FloorHeight_import", true)
+        imported_floor_height = OpenStudio::Measure::OSArgument::makeDoubleArgument("FloorHeight_import", false)
         imported_floor_height.setDisplayName("Importierte Raumhöhe")
         imported_floor_height.setUnits("m")
         args << imported_floor_height
 
-        air_changes = OpenStudio::Ruleset::OSArgument::makeDoubleArgument("ACH_Input_Parameter", true)
+        air_changes = OpenStudio::Measure::OSArgument::makeDoubleArgument("ACH_Input_Parameter", false)
         air_changes.setDisplayName("Luftwechselrate")
-        air_changes.setUnits("m^-1")
+        air_changes.setUnits("h^-1")
         args << air_changes
 
         return args
