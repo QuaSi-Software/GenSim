8583e825bb71a5accf4c8c37c1c3ff9643a6262b
Etienne Ott <etienne.ott@siz-energieplus.de>
2023-08-14 13:06:12 +0200

    Switch to custom measure tester in order to inject project-wide rubocop config.

--- Measures/testrunner.rb
+++ Measures/testrunner.rb
@@ -1,18 +1,14 @@
-require "openstudio_measure_tester"
+# frozen_string_literal: true
+
+require_relative "custom_measure_tester"
+
 measures_dir = Dir.pwd
 # all measures (recursively) from measures_dir will be tested
-
-runner = OpenStudioMeasureTester::Runner.new(measures_dir)
+runner = CustomRunner.new(measures_dir)
 
 # base_dir is needed for coverage results as they are written to disk on the at_exit calls
 base_dir = Dir.pwd
 
 result = runner.run_all(base_dir)
-puts result
 # result will be 0 or 1, 0=success, 1=failure
-
-# runner.run_style(false)
-
-runner.run_test(false, base_dir)
-
-# runner.run_rubocop(false)
+puts result
