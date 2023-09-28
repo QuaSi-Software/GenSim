2fb8292bf09d6089487f6ca954fce6debb5ee98d
Etienne Ott <etienne.ott@siz-energieplus.de>
2023-09-28 09:53:14 +0200

    Add command to create empty OSM file to CLI.

--- Measures/gensim_cli.rb
+++ Measures/gensim_cli.rb
@@ -16,6 +16,20 @@ class GenSimCLI < Thor
     ]
     system(arguments.join(" "))
   end
+
+  # Command to create an empty OSM file.
+  #
+  # @param file_name (String) The name of the OSM file to create
+  option :output_folder, :required => true, :default => "./Output"
+  desc "create_empty_osm --output_path=./Output FILE_NAME", "Create an empty OSM file"
+  def create_empty_osm(file_name="Model.osm")
+    File.open(File.join(options["output_folder"], file_name), "w") do |file|
+      file.write("OS:Version,\n")
+      file.write("  {0f20289d-c9f3-4775-8548-e6b6a77e899a}, !- Handle\n")
+      file.write("  2.5.0;                                  !- Version Identifier\n")
+      file.write("\n")
+    end
+  end
 end
 
 GenSimCLI.start(ARGV)
