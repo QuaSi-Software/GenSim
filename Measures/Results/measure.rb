45ad0cc960b9e90de4deca9639feac65016ce307
TobiasMaile <114696428+ProfMaile@users.noreply.github.com>
2024-06-06 08:46:21 +0200

    update results measure, debug flag, unit conversion and intensity results

--- Measures/Results/measure.rb
+++ Measures/Results/measure.rb
@@ -33,15 +33,22 @@ class Results < OpenStudio::Measure::ReportingMeasure
     args = OpenStudio::Measure::OSArgumentVector.new
 
     #make an argument for the reporting frequency
-    '''reporting_frequency_chs = OpenStudio::StringVector.new
-    reporting_frequency_chs << "Hourly"
-    reporting_frequency_chs << "Zone Timestep"
-    reporting_frequency_chs << "HVAC System Timestep"
-    reporting_frequency_chs << "All"'''
-    reporting_frequency = OpenStudio::Ruleset::OSArgument::makeStringArgument('ReportingFrequency', true)
-    reporting_frequency.setDisplayName("Reporting Frequency.")
-    reporting_frequency.setDefaultValue("Hourly")
-    args << reporting_frequency
+	args << OpenStudio::Measure::OSArgument::makeIntegerArgument("Timestep",true)
+	gross = OpenStudio::Measure::OSArgument::makeBoolArgument("IntensityResultsGross",true)
+	gross.setDefaultValue(false)
+	args << gross
+	gross_area = OpenStudio::Measure::OSArgument::makeDoubleArgument("GrossArea",true)
+	gross_area.setDefaultValue(1)
+	args << gross_area
+	netto = OpenStudio::Measure::OSArgument::makeBoolArgument("IntensityResultsNet",true)
+	netto.setDefaultValue(false)
+	args << netto
+	gross_net = OpenStudio::Measure::OSArgument::makeDoubleArgument("NetArea",true)
+	gross_net.setDefaultValue(1)
+	args << gross_net
+	debug = OpenStudio::Measure::OSArgument::makeBoolArgument("Debug",false)
+	debug.setDefaultValue(false)
+	args << debug
 
     return args
   end
@@ -73,59 +80,65 @@ class Results < OpenStudio::Measure::ReportingMeasure
     end
 	   return ann_env_pd
 	end
-	def saveToCSVFile(runner, output_timeseries, headers, csvFileName)
+
+	def saveToCSVFile(runner, output_timeseries, headers, conversion_factors, area, csvFileName)
 		csv_array = []
-  	csv_array << headers
+  		csv_array << headers
 
 		csv_array_annual = []
 		csv_array_annual << headers.drop(1)
 
-  	date_times = output_timeseries[output_timeseries.keys[0]][0].dateTimes
+  		date_times = output_timeseries[output_timeseries.keys[0]][0].dateTimes
 
-  	values = {}
+  		values = {}
 
-  	for key in output_timeseries.keys
-  		if output_timeseries.count > 1
-  			value = nil
-  			for timeseries in output_timeseries[key]
-  				if value.nil?
-  					value = timeseries.values
-  				else
-  					value += timeseries.values
-  				end
-  			end
-  			values[key] = value
-  		else
-  			values[key] = output_timeseries[key].values
-  		end
-		end
+		for key in output_timeseries.keys
+			if output_timeseries.count > 1
+				value = nil
+				for timeseries in output_timeseries[key]
+					if value.nil?
+						value = timeseries.values
+					else
+						value += timeseries.values
+					end
+				end
+				values[key] = value 
+			else
+				values[key] = output_timeseries[key].values
+			end
+			end
 
-  	num_times = date_times.size - 1
-  	for i in 0..num_times
-  		date_time = date_times[i]
-  		row = []
-  		row << date_time
-  		for key in headers[1..-1]
-  			value = values[key][i]
-  			row << value
-  		end
-  		csv_array << row
-		end
+		num_times = date_times.size - 1
+		for i in 0..num_times
+			date_time = date_times[i]
+			row = []
+			row << date_time
+			for key in headers[1..-1]
+				value = values[key][i]
+				if value.kind_of?(Array)
+					runner..registerInfo("Value is an array #{value}")
+				else
+					converted_value = value * conversion_factors[key] / area
+					row << converted_value
+				end
+			end
+			csv_array << row
+			end
 
-		row_annual = []
-		for key in headers[1..-1]
+			row_annual = []
+			for key in headers[1..-1]
 				sum = 0
 				for i in 0..num_times
 					sum += values[key][i]
 				end
-				row_annual << sum
-		end
-		csv_array_annual << row_annual
+				row_annual << sum * conversion_factors[key] / area
+			end
+			csv_array_annual << row_annual
 
-  	File.open("./report_variables_#{csvFileName}.csv", 'wb') do |file|
-  		csv_array.each do |elem|
-  			file.puts elem.join(',')
-  		end
+		File.open("./report_variables_#{csvFileName}.csv", 'wb') do |file|
+			csv_array.each do |elem|
+				file.puts elem.join(',')
+			end
 		end
 
 		File.open("./report_variables_#{csvFileName}-Sum.csv", 'wb') do |file|
@@ -134,7 +147,7 @@ class Results < OpenStudio::Measure::ReportingMeasure
 			end
 		end
 
-  	runner.registerInfo("Output file written to #{File.expand_path('.')}")
+		runner.registerInfo("Output file written to #{File.expand_path('.')}")
 		return csv_array_annual
 	end
 
@@ -148,55 +161,62 @@ class Results < OpenStudio::Measure::ReportingMeasure
     end
 
     #assign the user inputs to variables
-    reporting_frequency = runner.getStringArgumentValue("ReportingFrequency",user_arguments)
-	   reporting_frequency = "All"
+	timestep = runner.getIntegerArgumentValue("Timestep",user_arguments)
+	gross = runner.getBoolArgumentValue("IntensityResultsGross",user_arguments)
+	net = runner.getBoolArgumentValue("IntensityResultsNet",user_arguments)
+	gross_area = runner.getDoubleArgumentValue("GrossArea",user_arguments)
+	net_area = runner.getDoubleArgumentValue("NetArea",user_arguments)
+	debug = runner.getBoolArgumentValue("Debug",user_arguments)
+	
 
-	   list_of_variables = []
-	   list_of_variables << "DistrictCooling:Facility"
-	   list_of_variables << "DistrictHeating:Facility"
+	reporting_frequency = "All"
+
+	list_of_variables = []
+	list_of_variables << "DistrictCooling:Facility"
+	list_of_variables << "DistrictHeating:Facility"
 
 	# HEAT LOSS
-	   list_of_variables << "METER SURFACE AVERAGE FACE CONDUCTION HEAT LOSS RATE"
-	   list_of_variables << "METER SURFACE WINDOW HEAT LOSS ENERGY"
+	list_of_variables << "METER SURFACE AVERAGE FACE CONDUCTION HEAT LOSS RATE"
+	list_of_variables << "METER SURFACE WINDOW HEAT LOSS ENERGY"
 	# NEW VARIABLES
-	   list_of_variables << "METER SURFACE OUTSIDE FACE CONDUCTION HEAT LOSS RATE"
-	   list_of_variables << "Surface Outside Face Conduction Heat Loss Rate"
-	   list_of_variables << "Surface Average Face Conduction Heat Loss Rate"
-	   list_of_variables << "Surface Inside Face Conduction Heat Loss Rate"
+	list_of_variables << "METER SURFACE OUTSIDE FACE CONDUCTION HEAT LOSS RATE"
+	list_of_variables << "Surface Outside Face Conduction Heat Loss Rate"
+	list_of_variables << "Surface Average Face Conduction Heat Loss Rate"
+	list_of_variables << "Surface Inside Face Conduction Heat Loss Rate"
 
 	# INFILTRATION AND VENTILATION
-	   list_of_variables << "METER ZONE INFILTRATION HEAT LOSS"
-	   list_of_variables << "METER ZONE INFILTRATION HEAT GAIN"
-	   list_of_variables << "METER ZONE VENTILATION HEAT LOSS"
-	   list_of_variables << "METER ZONE VENTILATION HEAT GAIN"
+	list_of_variables << "METER ZONE INFILTRATION HEAT LOSS"
+	list_of_variables << "METER ZONE INFILTRATION HEAT GAIN"
+	list_of_variables << "METER ZONE VENTILATION HEAT LOSS"
+	list_of_variables << "METER ZONE VENTILATION HEAT GAIN"
 
 
 	# MECH LOSS
-	   list_of_variables << "METER MECHANICAL VENTILATION LOSS"
+	list_of_variables << "METER MECHANICAL VENTILATION LOSS"
 
 	# HEAT GAIN
-	   list_of_variables << "METER SURFACE AVERAGE FACE CONDUCTION HEAT GAIN RATE"
-	   list_of_variables << "METER SURFACE WINDOW HEAT GAIN ENERGY"
+	list_of_variables << "METER SURFACE AVERAGE FACE CONDUCTION HEAT GAIN RATE"
+	list_of_variables << "METER SURFACE WINDOW HEAT GAIN ENERGY"
 	# INFILTRATION AND VENTILATION
-	   list_of_variables << "METER ZONE ELECTRIC EQUIPMENT TOTAL HEATING ENERGY"
-	   list_of_variables << "METER ZONE LIGHTS TOTAL HEATING ENERGY"
-	   list_of_variables << "METER PEOPLE TOTAL HEATING ENERGY"
+	list_of_variables << "METER ZONE ELECTRIC EQUIPMENT TOTAL HEATING ENERGY"
+	list_of_variables << "METER ZONE LIGHTS TOTAL HEATING ENERGY"
+	list_of_variables << "METER PEOPLE TOTAL HEATING ENERGY"
 	# new variables
-	   list_of_variables << "METER SURFACE OUTSIDE FACE CONDUCTION HEAT GAIN RATE"
-	   list_of_variables << "METER MECHANICAL VENTILATION GAIN'"
-	   list_of_variables << "METER ZONE WINDOWS TOTAL TRANSMITTED SOLAR RADIATION ENERGY"
-	   list_of_variables << "Surface Outside Face Conduction Heat Gain Rate"
-	   list_of_variables << "Surface Average Face Conduction Heat Gain Rate"
-	   list_of_variables << "Surface Inside Face Conduction Heat Gain Rate"
+	list_of_variables << "METER SURFACE OUTSIDE FACE CONDUCTION HEAT GAIN RATE"
+	list_of_variables << "METER MECHANICAL VENTILATION GAIN'"
+	list_of_variables << "METER ZONE WINDOWS TOTAL TRANSMITTED SOLAR RADIATION ENERGY"
+	list_of_variables << "Surface Outside Face Conduction Heat Gain Rate"
+	list_of_variables << "Surface Average Face Conduction Heat Gain Rate"
+	list_of_variables << "Surface Inside Face Conduction Heat Gain Rate"
 
-	   sqlFile = getSQLFile(runner)
+	sqlFile = getSQLFile(runner)
 
-	   ann_env_pd = getEnvPeriod(runner, sqlFile)
+	ann_env_pd = getEnvPeriod(runner, sqlFile)
 
-	   runner.registerInfo("reporting frequency is #{reporting_frequency}")
+	runner.registerInfo("reporting frequency is #{reporting_frequency}")
 
-	   reporting_frequencies = {}
-	   if reporting_frequency == "All"
+	reporting_frequencies = {}
+	if reporting_frequency == "All"
    		reporting_frequencies = ["Hourly","Zone Timestep","HVAC System Timestep"]
    	else
    		reporting_frequencies << reporting_frequency
@@ -211,6 +231,8 @@ class Results < OpenStudio::Measure::ReportingMeasure
    		headers_filtered = ["#{reporting_frequency}"]
    		output_timeseries = {}
    		output_timeseries_filtered = {}
+		conversion_factors = {}
+		conversion_factors_filtered = {}
 
    		variable_names = sqlFile.availableVariableNames(ann_env_pd, reporting_frequency)
    		variable_names.each do |variable_name|
@@ -228,17 +250,35 @@ class Results < OpenStudio::Measure::ReportingMeasure
    				if !timeseries.empty?
    					timeseries = timeseries.get
    					units = timeseries.units
-   					headers << "#{variable_name.to_s}[#{units}]"
+					headerunits = units
+					if (units == "J") or (units == "W")
+						headerunits = "Wh"
+					end
+   					headers << "#{variable_name.to_s}[#{headerunits}]"
    					if bInit
    						output_timeseries[headers[-1]] = []
    					end
    					output_timeseries[headers[-1]] << timeseries
+					if units == "J"
+						conversion_factors[headers[-1]] = 1 / 3600
+					elsif units == "W"
+						conversion_factors[headers[-1]] = 1 / timestep
+					else
+						conversion_factors[headers[-1]] = 1
+					end
    					if list_of_variables.include? variable_name.to_s
-   						headers_filtered << "#{variable_name.to_s}[#{units}]"
+   						headers_filtered << "#{variable_name.to_s}[#{headerunits}]"
    						if bInit
    							output_timeseries_filtered[headers_filtered[-1]] = []
    						end
    						output_timeseries_filtered[headers_filtered[-1]] << timeseries
+						if units == "J"
+							conversion_factors_filtered[headers_filtered[-1]] = 1 / 3600
+						elsif units == "W"
+							conversion_factors_filtered[headers_filtered[-1]] = 1 / timestep
+						else
+							conversion_factors_filtered[headers_filtered[-1]] = 1
+						end
    					end
    					bInit = false
    				else
@@ -250,22 +290,39 @@ class Results < OpenStudio::Measure::ReportingMeasure
    		if output_timeseries.empty?
    			puts "No output variables found at reporting frequency = #{reporting_frequency}"
    			next
-			end
+		end
 
-			csvFileName = reporting_frequency.delete(' ')
-			data = saveToCSVFile(runner, output_timeseries, headers, csvFileName)
+		csvFileName = reporting_frequency.delete(' ')
+		if debug
+			data = saveToCSVFile(runner, output_timeseries, headers, conversion_factors, 1, csvFileName + "Debug")
+			if gross
+				data = saveToCSVFile(runner, output_timeseries, headers, conversion_factors, gross_area, csvFileName + "Debug-gross")
+			end
+			if net
+				data = saveToCSVFile(runner, output_timeseries, headers, conversion_factors, net_area, csvFileName + "Debug-net")
+			end
+		end
+
 
    		if output_timeseries_filtered.empty?
    			puts "No filtered output variables found at reporting frequency = #{reporting_frequency}"
    			next
-			end
+		end
 
-			data_annual = saveToCSVFile(runner, output_timeseries_filtered, headers_filtered, csvFileName + "filtered")
+		data_annual = saveToCSVFile(runner, output_timeseries_filtered, headers_filtered, conversion_factors_filtered, 1, csvFileName)
+		if gross
+			data = saveToCSVFile(runner, output_timeseries_filtered, headers_filtered, conversion_factors_filtered, gross_area, csvFileName + "-gross")
+		end
+		if net
+			data = saveToCSVFile(runner, output_timeseries_filtered, headers_filtered, conversion_factors_filtered, net_area, csvFileName + "-net")
+		end
    	end
+
+	
 
     # close the sql file
     sqlFile.close()
-	   runner.registerInfo("Closing the SQL file.")
+	runner.registerInfo("Closing the SQL file.")
 
     return true
   end
