5003b3a8ea00299ddd5ffb30dd950ede9645abf2
TobiasMaile <tobias@maileconsulting.de>
2025-09-08 11:19:03 +0200

    fixes #23
    adjusting heat exchanger input to reflect new curves

--- Measures/inject_radiant_surfaces_idf/measure.rb
+++ Measures/inject_radiant_surfaces_idf/measure.rb
@@ -74,9 +74,8 @@ class InjectRadiantSurfacesIDF < OpenStudio::Measure::EnergyPlusMeasure
     runner.registerInitialCondition("The building started with #{lowTempRadiants.size} Low Temp Radiant objects and #{internalMasses.size} Internal Masses.")
 
     current_version = OpenStudio::VersionString.new(OpenStudio.openStudioVersion())
-    required_version = OpenStudio::VersionString.new(3,2,0)
 
-    if current_version >= required_version
+    if current_version >= OpenStudio::VersionString.new(3,2,0)
       runner.registerInfo("Found version #{current_version.to_s()} >= 3.2.0")
       # since version 3.2 the low temp radiant object do not get propertly converted into IDF so this code will fix it
       # init the dictionary
