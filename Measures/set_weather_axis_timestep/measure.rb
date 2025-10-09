e238d3cf6b290c243f4741418a4ffb29f0138f70
Etienne Ott <etienne.ott@siz-energieplus.de>
2025-10-09 16:23:14 +0200

    Restructure measure set_weather_axis_timestep to implement three different sizing methods.
    The first is the old implementation of using a DDY file.
    The second uses automatic determination of design days from the weather data. This isn't working yet.
    The third uses manually specified design days. This is somewhat worse than both options but has the advantage that it avoids the need for DDY files and it's working reasonably well.

--- Measures/set_weather_axis_timestep/measure.rb
+++ Measures/set_weather_axis_timestep/measure.rb
@@ -23,13 +23,156 @@ class SetWeatherAxisTimestep < OpenStudio::Measure::ModelMeasure
 
     args << OpenStudio::Measure::OSArgument.makeStringArgument("weather_file_path", true)
     args << OpenStudio::Measure::OSArgument.makeIntegerArgument("time_step", true)
+
     northaxis = OpenStudio::Measure::OSArgument.makeDoubleArgument("north_axis", true)
     northaxis.setDefaultValue(-9999.0)
     args << northaxis
-    replace_design_days = OpenStudio::Measure::OSArgument.makeBoolArgument("replace_design_days", true)
-    replace_design_days.setDefaultValue(true)  # <- this sets the default value
-    args << replace_design_days
+
+    sizing_method = OpenStudio::Measure::OSArgument.makeChoiceArgument(
+      "sizing_method",
+      ["use_ddy_file", "manual_design_days", "automatic_design_days"],
+      ["Use DDY file", "Manual Design Days", "Automatic Design Days"],
+      true
+    )
+    sizing_method.setDefaultValue("manual_design_days")
+    args << sizing_method
+
     return args
+  end
+
+  # sizing method using the DDY file that is expected to be along the EPW weather file
+  def sizing_use_ddy_file(model, runner, epw_file)
+    # find the ddy files
+    ddy_file = "#{File.join(File.dirname(epw_file.path.to_s), File.basename(epw_file.path.to_s, '.*'))}.ddy"
+    runner.registerInfo("Looking for ddy file. #{ddy_file}")
+    unless File.exist? ddy_file
+      ddy_files = Dir["#{File.dirname(epw_file.path.to_s)}/*.ddy"]
+      if ddy_files.size > 1
+        runner.registerError("More than one ddy file in the EPW directory")
+        return false
+      end
+      if ddy_files.empty?
+        runner.registerError("could not find the ddy file in the EPW directory")
+        return false
+      end
+
+      ddy_file = ddy_files.first
+    end
+
+    unless ddy_file
+      runner.registerError "Could not find DDY file for #{ddy_file}"
+      return error
+    end
+
+    ddy_model = OpenStudio::EnergyPlus.loadAndTranslateIdf(ddy_file).get
+    ddy_model.getObjectsByType("OS:SizingPeriod:DesignDay".to_IddObjectType).each do |d|
+      # grab only the ones that matter
+      ddy_list = /(Htg 99.6)|(Clg .4)/
+      if d.name.get =~ ddy_list
+        runner.registerInfo("Adding object #{d.name}")
+
+        # add the object to the existing model
+        model.addObject(d.clone)
+        runner.registerInfo("Adding design day #{d.name}.")
+      end
+    end
+  end
+
+  # sizing method using design days automatically determined from the weather data
+  def sizing_automatic_design_days(model, runner, epw_file)
+    idf_snippet = <<-IDF
+SizingPeriod:WeatherFileConditionType,
+Extreme Summer Weather Period,     !- Name
+Extremes,                          !- Period Selection
+Monday,                            !- Day of Week for Start Day
+Yes,                               !- Use Weather File Daylight Saving Period
+Yes;                               !- Use Weather File Rain and Snow Indicators
+
+SizingPeriod:WeatherFileConditionType,
+Extreme Winter Weather Period,     !- Name
+Extremes,                          !- Period Selection
+Monday,                            !- Day of Week for Start Day
+Yes,                               !- Use Weather File Daylight Saving Period
+Yes;                               !- Use Weather File Rain and Snow Indicators
+  IDF
+
+    # load the IDF text as an IdfObject (returns Optional in C++)
+    optional_idf_obj = OpenStudio::IdfObject.load(idf_snippet)
+    unless optional_idf_obj.is_initialized
+      runner.registerError("Failed to parse IDF snippet")
+      return false
+    end
+
+    idf_obj = optional_idf_obj.get
+    model.addObject(idf_obj)
+    runner.registerInfo("Added 2 SizingPeriod:WeatherFileConditionType objects to the model.")
+
+    # summer_wfc = OpenStudio::Model::WeatherFileConditionType.new(model)
+    # summer_wfc.setName("Sizing - Extreme Summer")
+    # summer_wfc.setAttribute('Control Type', 'Extremes')
+    # summer_wfc.setAttribute('Condition Type', 'Temperature')
+
+    # winter_wfc = OpenStudio::Model::WeatherFileConditionType.new(model)
+    # winter_wfc.setName("Sizing - Extreme Winter")
+    # winter_wfc.setAttribute('Control Type', 'Extremes')
+    # winter_wfc.setAttribute('Condition Type', 'Temperature')
+
+    # # Create Summer Extreme Sizing Period
+    # summer_extreme = OpenStudio::Model::WeatherFileConditionType.new(model)
+    # summer_extreme.setName("Summer Extreme")
+    # summer_extreme.setWeatherFileConditionType("SummerExtreme")
+
+    # # Create Winter Extreme Sizing Period
+    # winter_extreme = OpenStudio::Model::WeatherFileConditionType.new(model)
+    # winter_extreme.setName("Winter Extreme")
+    # winter_extreme.setWeatherFileConditionType("WinterExtreme")
+  end
+
+  # sizing method using manually specified design days
+  def sizing_manual_design_days(model, runner, epw_file)
+    # remove existing design days
+    model.getDesignDays.each(&:remove)
+
+    # summer design day
+    summer_dd = OpenStudio::Model::DesignDay.new(model)
+    summer_dd.setName('Summer Design Day')
+    summer_dd.setMaximumDryBulbTemperature(38.0)
+    summer_dd.setDailyDryBulbTemperatureRange(10.0)
+    summer_dd.setBarometricPressure(101325.0)
+    summer_dd.setHumidityConditionType('HumidityRatio')
+    summer_dd.setHumidityRatioAtMaximumDryBulb(0.012) # mass ratio water / dry_air
+    summer_dd.setDayType('SummerDesignDay')
+    summer_dd.setMonth(7)
+    summer_dd.setDayOfMonth(21)
+    summer_dd.setDryBulbTemperatureRangeModifierType('DefaultMultipliers')
+    summer_dd.setSkyClearness(1.0)
+    summer_dd.setRainIndicator(false)
+    summer_dd.setSnowIndicator(false)
+    summer_dd.setWindSpeed(1.0)
+    summer_dd.setWindDirection(180)
+    summer_dd.setSolarModelIndicator('ASHRAEClearSky')
+
+    # winter design day
+    winter_dd = OpenStudio::Model::DesignDay.new(model)
+    winter_dd.setName('Winter Design Day')
+    winter_dd.setMaximumDryBulbTemperature(-13.0)
+    winter_dd.setDailyDryBulbTemperatureRange(0.0)
+    winter_dd.setBarometricPressure(98675.0)
+    winter_dd.setHumidityConditionType('Wetbulb')
+    winter_dd.setWetBulbOrDewPointAtMaximumDryBulb(-15.0)
+    winter_dd.setDayType('WinterDesignDay')
+    winter_dd.setMonth(1)
+    winter_dd.setDayOfMonth(21)
+    winter_dd.setDryBulbTemperatureRangeModifierType('DefaultMultipliers')
+    winter_dd.setSkyClearness(0.0)
+    winter_dd.setRainIndicator(false)
+    winter_dd.setSnowIndicator(true)
+    winter_dd.setWindSpeed(15.0)
+    winter_dd.setWindDirection(0)
+    winter_dd.setSolarModelIndicator('ASHRAEClearSky')
+
+    runner.registerInfo("Added design day: #{summer_dd.nameString}")
+    runner.registerInfo("Added design day: #{winter_dd.nameString}")
   end
 
   # define what happens when the measure is run
@@ -42,9 +185,10 @@ class SetWeatherAxisTimestep < OpenStudio::Measure::ModelMeasure
     weatherFilePath = runner.getStringArgumentValue("weather_file_path", user_arguments)
     northAxis = runner.getDoubleArgumentValue("north_axis", user_arguments)
     timestep = runner.getIntegerArgumentValue("time_step", user_arguments)
-    replace_design_days = runner.getBoolArgumentValue("replace_design_days", user_arguments)
+    sizing_method = runner.getStringArgumentValue("sizing_method", user_arguments)
 
-    # Add Weather File
+    # read weather File and set site data
+    epw_file = false
     if File.exist?(weatherFilePath) && weatherFilePath.downcase.include?(".epw")
       epw_file = OpenStudio::EpwFile.new(weatherFilePath)
 
@@ -54,66 +198,32 @@ class SetWeatherAxisTimestep < OpenStudio::Measure::ModelMeasure
       weather_time = epw_file.timeZone
       weather_elev = epw_file.elevation
 
-      # Add or update site data
       site = model.getSite
       site.setName(weather_name)
       site.setLatitude(weather_lat)
       site.setLongitude(weather_lon)
       site.setTimeZone(weather_time)
       site.setElevation(weather_elev)
-
-      runner.registerInfo("Setting site data.")
-
-      if(!replace_design_days)
-          # find the ddy files
-          ddy_file = "#{File.join(File.dirname(epw_file.path.to_s), File.basename(epw_file.path.to_s, '.*'))}.ddy"
-          runner.registerInfo("Looking for ddy file. #{ddy_file}")
-          unless File.exist? ddy_file
-            ddy_files = Dir["#{File.dirname(epw_file.path.to_s)}/*.ddy"]
-            if ddy_files.size > 1
-              runner.registerError("More than one ddy file in the EPW directory")
-              return false
-            end
-            if ddy_files.empty?
-              runner.registerError("could not find the ddy file in the EPW directory")
-              return false
-            end
-
-            ddy_file = ddy_files.first
-          end
-
-          unless ddy_file
-            runner.registerError "Could not find DDY file for #{ddy_file}"
-            return error
-          end
-
-          ddy_model = OpenStudio::EnergyPlus.loadAndTranslateIdf(ddy_file).get
-          ddy_model.getObjectsByType("OS:SizingPeriod:DesignDay".to_IddObjectType).each do |d|
-            # grab only the ones that matter
-            ddy_list = /(Htg 99.6)|(Clg .4)/
-            if d.name.get =~ ddy_list
-              runner.registerInfo("Adding object #{d.name}")
-
-              # add the object to the existing model
-              model.addObject(d.clone)
-              runner.registerInfo("Adding design day #{d.name}.")
-            end
-          end
-      else
-            # Create Summer Extreme Sizing Period
-            summer_extreme = OpenStudio::Model::WeatherFileConditionType.new(model)
-            summer_extreme.setName("Summer Extreme")
-            # summer_extreme.setWeatherFileConditionType("SummerExtreme")
-
-            # Create Winter Extreme Sizing Period
-            winter_extreme = OpenStudio::Model::WeatherFileConditionType.new(model)
-            winter_extreme.setName("Winter Extreme")
-            # winter_extreme.setWeatherFileConditionType("WinterExtreme")
-      end
+      runner.registerInfo(
+        "Set site data as '#{weather_name}' at lat #{weather_lat} lon #{weather_lon} " + 
+        "elev #{weather_elev} and timezone #{weather_time}."
+      )
     else
       runner.registerInfo("'#{weatherFilePath}' does not exist or is not an .epw file.")
     end
 
+    # perform sizing depending on chosen method
+    if epw_file && sizing_method == "use_ddy_file"
+      sizing_use_ddy_file(model, runner, epw_file)
+    elsif epw_file && sizing_method == "automatic_design_days"
+      sizing_automatic_design_days(model, runner, epw_file)
+    elsif sizing_method == "manual_design_days"
+      sizing_manual_design_days(model, runner, epw_file)
+    else
+      runner.registerError("Required weather file missing or unknown sizing method #{sizing_method}.")
+    end
+
+    # set north axis of building
     if northAxis != -9999.0
       building = model.getBuilding
       building.setNorthAxis(northAxis)
